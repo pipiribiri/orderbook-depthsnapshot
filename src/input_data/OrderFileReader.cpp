fd030abdd28d44a7faac23d551037a8dd80e5d1b
marco <aa@aa.es>
2025-12-09 21:11:51 +0100

    Add check for empty read messages in the file

--- src/input_data/OrderFileReader.cpp
+++ src/input_data/OrderFileReader.cpp
@@ -18,32 +18,36 @@ void OrderFileReader::start() const
 
 bool OrderFileReader::next(InputOrderMessage &retOrder, uint32_t& retSequenceNumber)
 {
-    if (!m_inputFile.good()) return false;
-
-    retSequenceNumber = InputOrderParser::readSequenceNo(m_inputFile);
-    const uint32_t messageSize = InputOrderParser::readMessageSize(m_inputFile);
-    (void)messageSize; // not used directly, but could validate
-
-    retOrder.type    = InputOrderParser::readOrderType(m_inputFile);
-    retOrder.symbol  = InputOrderParser::readSymbol(m_inputFile);
-    retOrder.orderId = InputOrderParser::readOrderId(m_inputFile);
-    retOrder.side    = InputOrderParser::readSide(m_inputFile);
-    InputOrderParser::skipReserved(m_inputFile, 3);
-
-    retOrder.volume = 0;
-    retOrder.price = 0;
-    retOrder.tradedQuantity = 0;
-
-    if (retOrder.type == OrderType::EXECUTE)
+    bool ret = false;
+    if (m_inputFile.good())
     {
-        retOrder.tradedQuantity = InputOrderParser::readTradedQuantity(m_inputFile);
+        retSequenceNumber = InputOrderParser::readSequenceNo(m_inputFile);
+
+        if (const uint32_t messageSize = InputOrderParser::readMessageSize(m_inputFile); messageSize != 0)
+        {
+            retOrder.type    = InputOrderParser::readOrderType(m_inputFile);
+            retOrder.symbol  = InputOrderParser::readSymbol(m_inputFile);
+            retOrder.orderId = InputOrderParser::readOrderId(m_inputFile);
+            retOrder.side    = InputOrderParser::readSide(m_inputFile);
+            InputOrderParser::skipReserved(m_inputFile, 3);
+
+            retOrder.volume = 0;
+            retOrder.price = 0;
+            retOrder.tradedQuantity = 0;
+
+            if (retOrder.type == OrderType::EXECUTE)
+            {
+                retOrder.tradedQuantity = InputOrderParser::readTradedQuantity(m_inputFile);
+            }
+            else if (retOrder.type == OrderType::ADD || retOrder.type == OrderType::UPDATE)
+            {
+                retOrder.volume  = InputOrderParser::readVolume(m_inputFile);
+                retOrder.price = InputOrderParser::readPrice(m_inputFile);
+                InputOrderParser::skipReserved(m_inputFile, 4);
+            }
+            ret = true;
+        }
     }
-    else if (retOrder.type == OrderType::ADD || retOrder.type == OrderType::UPDATE)
-    {
-        retOrder.volume  = InputOrderParser::readVolume(m_inputFile);
-        retOrder.price = InputOrderParser::readPrice(m_inputFile);
-        InputOrderParser::skipReserved(m_inputFile, 4);
-    }
-   
-    return true;
+
+    return ret;
 }
